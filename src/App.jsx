e46971f4eb126dce49a87c4e5545527e0b42fc64
Nhlanhla Khumalo <ntg567497@gmail.com>
2026-02-26 16:07:42 +0200

    implement dock feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import {Navbar, Welcome} from "#components";
+import {Navbar, Welcome, Dock} from "#components";
 
 const App = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
             <Welcome />
+            <Dock />
         </div>
     );
 };
