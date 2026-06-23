8f3acfd77558e45a8232f6461f6beb0b5f013897
Nhlanhla Khumalo <ntg567497@gmail.com>
2026-06-23 21:25:23 +0200

    implemented project updates

--- src/windows/Safari.jsx
+++ src/windows/Safari.jsx
@@ -39,7 +39,7 @@ const Safari = () => {
                 </div>
                 </div>
             <div className="blog">
-                <h2>My Developer Blog</h2>
+                <h2>Blogs</h2>
 
                 <div className="space-y-8">
                     {blogPosts.map(({id, image, title, date, link}) => (
